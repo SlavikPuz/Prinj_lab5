7a15f21be440dbf44801a67b22162199a2f66710
SlavikPuz <slavikpuz72ru@gmail.com>
2025-11-06 13:20:40 +0300

    P.py

--- P.py
+++ P.py
@@ -1,17 +1,20 @@
-from core import survival_percent, young_old_survival_by_class
+from core import young_old_survival_by_class
 import streamlit as st
 import pandas as pd
 from PIL import Image
+
 
 st.title("Анализ выживших пассажиров Титаника по возрастным группам")
 
 image = Image.open("titanic.jpeg")
 st.image(image, use_container_width=True)
 
+
 @st.cache_data
 def load_data():
     df = pd.read_csv("titanic_train.csv")
     return df
+
 
 data = load_data()
 
@@ -24,10 +27,13 @@ young_percent, old_percent = young_old_survival_by_class(data, selected_class)
 
 st.subheader("Результаты анализа")
 col1, col2 = st.columns(2)
+
 with col1:
     st.metric("Процент выживших молодых (<30)", f"{young_percent:.2f}%")
 with col2:
     st.metric("Процент выживших старых (>60)", f"{old_percent:.2f}%")
 
 st.subheader("Отфильтрованные данные по выбранному классу")
-st.dataframe(data.loc[data["Pclass"] == selected_class, ["Name", "Age", "Pclass", "Survived"]].dropna())
+st.dataframe(
+    data.loc[data["Pclass"] == selected_class, ["Name", "Age", "Pclass", "Survived"]].dropna()
+)
